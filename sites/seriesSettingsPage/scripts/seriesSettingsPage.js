80a7fa3c52ab69e659c93806f8330d407155d268
Christian Ramelow <info@codekandis.net>
2021-01-06 12:51:42 +0100

    Adds the class `SeriesSettingsPage`.

---
+++ sites/seriesSettingsPage/scripts/seriesSettingsPage.js
@@ -0,0 +1,45 @@
+class SeriesSettingsPage
+{
+	constructor( settings )
+	{
+		this._settings      = settings;
+		this._episodes      = new Episodes( '#waste1 li, #waste2 li', this._episodeNameHandler );
+		this._apiController = new ApiController(
+			this._settings.get( 'apiBaseUri' ),
+			this._settings.get( 'apiUserId' ),
+			this._settings.get( 'apiKey' )
+		);
+	}
+
+	_episodeNameHandler( container )
+	{
+		return container
+			.textContent
+			.trim()
+			.toLowerCase();
+	}
+
+	_filterEpisodes()
+	{
+		return ( new EpisodesFilter( this._episodes, this._apiController, true ) )
+			.filter();
+	}
+
+	_addActions()
+	{
+		( new ActionAdder( this._episodes, this._apiController, 'afterbegin', true ) )
+			.addActions()
+	}
+
+	execute()
+	{
+		this
+			._filterEpisodes()
+			.then(
+				() =>
+				{
+					this._addActions();
+				}
+			);
+	}
+}
