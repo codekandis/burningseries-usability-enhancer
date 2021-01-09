bdef45d2b710e47c81e711f5759e329973a93de4
Christian Ramelow <info@codekandis.net>
2021-01-09 13:38:57 +0100

    Updates the class `ApiUris`.

--- library/scripts/ApiUris.js
+++ library/scripts/ApiUris.js
@@ -3,16 +3,16 @@ class ApiUris
 	constructor( baseUri, userId )
 	{
 		this._relativeUris = {
-			userSeriesDenials: '/users/{{userId}}/series-denials'
+			userSeriesDenialsFiltered: '/users/{{userId}}/series-denials/filtered'
 		};
 
 		this._baseUri = baseUri;
 		this._userId  = userId;
 	}
 
-	get userSeriesDenials()
+	get userSeriesDenialsFiltered()
 	{
-		return this._baseUri + this._replaceUriPlaceHolders( this._relativeUris.userSeriesDenials );
+		return this._baseUri + this._replaceUriPlaceHolders( this._relativeUris.userSeriesDenialsFiltered );
 	}
 
 	_replaceUriPlaceHolders( uri )
