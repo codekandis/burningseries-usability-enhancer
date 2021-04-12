fc68c24b941d4bff46f820a745f3062919eedf90
Christian Ramelow <info@codekandis.net>
2021-04-12 04:29:34 +0200

    Updates the class `AllPages`.

--- sites/allPages/scripts/AllPages.js
+++ sites/allPages/scripts/AllPages.js
@@ -3,11 +3,13 @@ class AllPages
 	constructor( settings )
 	{
 		this._settings    = settings;
+		this._mouseMarker = new MouseMarker();
 		this._menuHandler = new MenuHandler( '#other-series-nav' );
 	}
 
 	execute()
 	{
+		this._mouseMarker.markMouse();
 		this._menuHandler.handle();
 	}
 }
