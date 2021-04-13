2bd60f4bc11fb4d050371693fbbe9e3c1182f32e
Christian Ramelow <info@codekandis.net>
2021-04-13 13:19:43 +0200

    Updates the class `EpisodesNavigator`.

--- library/scripts/EpisodesNavigator.js
+++ library/scripts/EpisodesNavigator.js
@@ -14,22 +14,124 @@ class EpisodesNavigator
 		this._navigator.appendChild( this._buttons );
 	}
 
-	_addEvents( buttons )
+	async _getEnclosingEpisodesOfSeason( uri )
 	{
-		const episodesContainer   = document.querySelector( '#episodes ul' );
-		const episodes            = episodesContainer.querySelectorAll( 'li' );
-		const currentEpisode      = episodesContainer.querySelector( 'li.active' );
-		const currentEpisodeIndex = Array.prototype.indexOf.call( episodes, currentEpisode );
+		return await new Promise(
+			( resolveHandler, rejectHandler ) =>
+			{
+				( new BsToController() )
+					.readEpisodes( uri )
+					.then(
+						( responseData ) =>
+						{
+							const episodes = [ ...responseData.querySelectorAll( 'table.episodes tbody tr:first-child td:nth-child(1) a, table.episodes tbody tr:last-child td:nth-child(1) a' ) ]
+								.map(
+									( element ) =>
+									{
+										return element.href;
+									}
+								)
+							resolveHandler(
+								{
+									first: episodes[ 0 ],
+									last:  episodes[ 1 ],
+								}
+							);
+						}
+					);
+			}
+		);
+	}
 
-		if ( 0 === currentEpisodeIndex )
-		{
-			buttons.previousEpisode.addEventListener(
-				'click',
-				( event ) =>
+	get _seasons()
+	{
+		const seasonsContainer   = document.querySelector( '#seasons ul' );
+		const seasons            = [ ...seasonsContainer.querySelectorAll( 'li a' ) ]
+			.map(
+				( element ) =>
 				{
-					event.preventDefault();
+					return element.href;
 				}
 			);
+		const currentSeasonIndex = seasons.indexOf(
+			seasonsContainer.querySelector( 'li.active a' ).href
+		);
+
+		return {
+			list:         seasons,
+			currentIndex: currentSeasonIndex
+		}
+	}
+
+	get _episodes()
+	{
+		const episodesContainer   = document.querySelector( '#episodes ul' );
+		const episodes            = [ ...episodesContainer.querySelectorAll( 'li a' ) ]
+			.map(
+				( element ) =>
+				{
+					return element.href;
+				}
+			);
+		const currentEpisodeIndex = episodes.indexOf(
+			episodesContainer.querySelector( 'li.active a' ).href
+		);
+
+		return {
+			list:         episodes,
+			currentIndex: currentEpisodeIndex
+		}
+	}
+
+	_navigateBackward( seasons, episodes )
+	{
+		if ( 0 !== episodes.currentIndex )
+		{
+			window.location.href = episodes.list[ episodes.currentIndex - 1 ];
+
+			return;
+		}
+
+		this._getEnclosingEpisodesOfSeason( seasons.list[ seasons.currentIndex - 1 ] )
+			.then(
+				( enlosingEpisodes ) =>
+				{
+					window.location.href = enlosingEpisodes.last;
+				}
+			);
+	}
+
+	_navigateForward( seasons, episodes )
+	{
+		if ( episodes.list.length - 1 !== episodes.currentIndex )
+		{
+			window.location.href = episodes.list[ episodes.currentIndex + 1 ];
+
+			return;
+		}
+
+		this._getEnclosingEpisodesOfSeason( seasons.list[ seasons.currentIndex + 1 ] )
+			.then(
+				( enlosingEpisodes ) =>
+				{
+					window.location.href = enlosingEpisodes.first;
+				}
+			);
+	}
+
+	_addEvents( buttons )
+	{
+		const nullHandler = ( event ) =>
+		{
+			event.preventDefault();
+		};
+
+		const seasons  = this._seasons;
+		const episodes = this._episodes;
+
+		if ( 0 === seasons.currentIndex && 0 === episodes.currentIndex )
+		{
+			buttons.previousEpisode.addEventListener( 'click', nullHandler );
 			buttons.previousEpisode.classList.add( 'disabled' );
 		}
 		else
@@ -39,21 +141,14 @@ class EpisodesNavigator
 				( event ) =>
 				{
 					event.preventDefault();
-					const targetEpisodeIndex = currentEpisodeIndex - 1;
-					window.location.href     = episodesContainer.querySelectorAll( 'li a' )[ targetEpisodeIndex ].href;
+					this._navigateBackward( seasons, episodes );
 				}
 			);
 		}
 
-		if ( episodes.length - 1 === currentEpisodeIndex )
+		if ( seasons.list.length - 1 === seasons.currentIndex && episodes.list.length - 1 === episodes.currentIndex )
 		{
-			buttons.nextEpisode.addEventListener(
-				'click',
-				( event ) =>
-				{
-					event.preventDefault();
-				}
-			);
+			buttons.nextEpisode.addEventListener( 'click', nullHandler );
 			buttons.nextEpisode.classList.add( 'disabled' );
 		}
 		else
@@ -63,8 +158,7 @@ class EpisodesNavigator
 				( event ) =>
 				{
 					event.preventDefault();
-					const targetEpisodeIndex = currentEpisodeIndex + 1;
-					window.location.href     = episodesContainer.querySelectorAll( 'li a' )[ targetEpisodeIndex ].href;
+					this._navigateForward( seasons, episodes );
 				}
 			);
 		}
@@ -79,7 +173,7 @@ class EpisodesNavigator
 
 		const buttons = {
 			previousEpisode: DomHelper.createElementFromString( '<li><a href="#">Previous</a></li>' ),
-			nextEpisode:     DomHelper.createElementFromString( '<li><a href="#">Next</a></li>' ),
+			nextEpisode:     DomHelper.createElementFromString( '<li><a href="#">Next</a></li>' )
 		};
 
 		buttons.forEach(
