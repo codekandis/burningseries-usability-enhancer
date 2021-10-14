ae9638d8e723035889c8aa2d54c215ad85e839fa
Christian Ramelow <info@codekandis.net>
2021-10-14 23:10:49 +0200

    Adds a menu reorderer.

---
+++ library/scripts/bs.to/MenuReorderer.js
@@ -0,0 +1,21 @@
+class MenuReorderer
+{
+	constructor( orderPresets )
+	{
+		this._orderPresets = orderPresets;
+	}
+
+	reorder()
+	{
+		this._orderPresets.forEach(
+			( orderPreset ) =>
+			{
+				DomHelper.insert(
+					document.querySelector( orderPreset.targetSelector ),
+					document.querySelector( orderPreset.selector ),
+					orderPreset.position
+				)
+			}
+		);
+	}
+}
