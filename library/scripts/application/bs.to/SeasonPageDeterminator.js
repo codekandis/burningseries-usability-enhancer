374fb5a04c10365a2cf30c3bf881f342ac4f4725
Christian Ramelow <info@codekandis.net>
2022-03-16 08:06:45 +0100

    Enhances the season page determinator with additional domains.

--- library/scripts/application/bs.to/SeasonPageDeterminator.js
+++ library/scripts/application/bs.to/SeasonPageDeterminator.js
@@ -12,7 +12,25 @@ class SeasonPageDeterminator extends BaseClass
 			/^https:\/\/bs\.to\/serie\/[0-9a-z\-]+\/\d+\/[a-z]+\/?$/i,
 			/^https:\/\/burningseries\.co\/serie\/[0-9a-z\-]+\/?$/i,
 			/^https:\/\/burningseries\.co\/serie\/[0-9a-z\-]+\/\d+\/?$/i,
-			/^https:\/\/burningseries\.co\/serie\/[0-9a-z\-]+\/\d+\/[a-z]+\/?$/i
+			/^https:\/\/burningseries\.co\/serie\/[0-9a-z\-]+\/\d+\/[a-z]+\/?$/i,
+			/^https:\/\/burningseries\.sx\/serie\/[0-9a-z\-]+\/?$/i,
+			/^https:\/\/burningseries\.sx\/serie\/[0-9a-z\-]+\/\d+\/?$/i,
+			/^https:\/\/burningseries\.sx\/serie\/[0-9a-z\-]+\/\d+\/[a-z]+\/?$/i,
+			/^https:\/\/burningseries\.ac\/serie\/[0-9a-z\-]+\/?$/i,
+			/^https:\/\/burningseries\.ac\/serie\/[0-9a-z\-]+\/\d+\/?$/i,
+			/^https:\/\/burningseries\.ac\/serie\/[0-9a-z\-]+\/\d+\/[a-z]+\/?$/i,
+			/^https:\/\/burningseries\.vc\/serie\/[0-9a-z\-]+\/?$/i,
+			/^https:\/\/burningseries\.vc\/serie\/[0-9a-z\-]+\/\d+\/?$/i,
+			/^https:\/\/burningseries\.vc\/serie\/[0-9a-z\-]+\/\d+\/[a-z]+\/?$/i,
+			/^https:\/\/burningseries\.cx\/serie\/[0-9a-z\-]+\/?$/i,
+			/^https:\/\/burningseries\.cx\/serie\/[0-9a-z\-]+\/\d+\/?$/i,
+			/^https:\/\/burningseries\.cx\/serie\/[0-9a-z\-]+\/\d+\/[a-z]+\/?$/i,
+			/^https:\/\/burningseries\.nz\/serie\/[0-9a-z\-]+\/?$/i,
+			/^https:\/\/burningseries\.nz\/serie\/[0-9a-z\-]+\/\d+\/?$/i,
+			/^https:\/\/burningseries\.nz\/serie\/[0-9a-z\-]+\/\d+\/[a-z]+\/?$/i,
+			/^https:\/\/burningseries\.se\/serie\/[0-9a-z\-]+\/?$/i,
+			/^https:\/\/burningseries\.se\/serie\/[0-9a-z\-]+\/\d+\/?$/i,
+			/^https:\/\/burningseries\.se\/serie\/[0-9a-z\-]+\/\d+\/[a-z]+\/?$/i
 		];
 		this._uri      = uri;
 	}
@@ -22,7 +40,7 @@ class SeasonPageDeterminator extends BaseClass
 		return this._patterns.some(
 			( regularExpression ) =>
 			{
-				return regularExpression.test( window.location.href );
+				return regularExpression.test( this._uri );
 			}
 		);
 	}
