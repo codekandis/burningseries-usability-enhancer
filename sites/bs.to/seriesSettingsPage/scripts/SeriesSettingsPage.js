d0a7e485505601a7ea20f8c4a50f1e8c3b2a7129
Christian Ramelow <info@codekandis.net>
2022-03-29 23:16:10 +0200

    Removes the action buttons from the series settings page.

--- sites/bs.to/seriesSettingsPage/scripts/SeriesSettingsPage.js
+++ sites/bs.to/seriesSettingsPage/scripts/SeriesSettingsPage.js
@@ -6,16 +6,13 @@ class SeriesSettingsPage extends BaseClass
 	{
 		super();
 
-		this._settings          = settings;
-		this._apiController     = new ApiController(
+		this._settings      = settings;
+		this._apiController = new ApiController(
 			this._settings.get( 'apiBaseUri' ),
 			this._settings.get( 'apiUserId' ),
 			this._settings.get( 'apiKey' )
 		);
-		this._episodes          = new Episodes( '#usrCnt li', this._episodeNameHandler, this._episodeUriHandler );
-		this._denialsFilter     = new DenialsFilter( this._episodes, this._apiController, true );
-		this._favoritesSwitcher = new FavoritesSwitcher( this._episodes, this._apiController );
-		this._interestsSwitcher = new InterestsSwitcher( this._episodes, this._apiController );
+		this._episodes      = new Episodes( '#usrCnt li', this._episodeNameHandler, this._episodeUriHandler );
 	}
 
 	get _episodeNameHandler()
@@ -42,33 +39,8 @@ class SeriesSettingsPage extends BaseClass
 		return this._denialsFilter.filter();
 	}
 
-	_switchFavorites()
-	{
-		this._favoritesSwitcher.switch();
-	}
-
-	_switchInterests()
-	{
-		this._interestsSwitcher.switch();
-	}
-
-	_addActions( denialsFilter, favoritesSwitcher, interestsSwitcher )
-	{
-		( new ActionAdder( this._episodes, this._apiController, DomInsertPositions.AFTER_BEGIN, denialsFilter, favoritesSwitcher, interestsSwitcher ) )
-			.addActions();
-	}
-
 	execute()
 	{
-		this
-			._filterDenials()
-			.then(
-				( denialsFilter ) =>
-				{
-					this._addActions( denialsFilter, this._favoritesSwitcher, this._interestsSwitcher );
-					this._switchFavorites();
-					this._switchInterests();
-				}
-			);
+		this._filterDenials();
 	}
 }
