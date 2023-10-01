fe3f2130f0bf7c1c66c560f7bb55207dba332862
Christian Ramelow <info@codekandis.net>
2023-10-01 12:56:35 +0200

    Adds the `WatchedLoader`.

---
+++ library/scripts/application/bs.to/WatchedLoader.js
@@ -0,0 +1,43 @@
+'use strict';
+
+class WatchedLoader extends BaseClass
+{
+	constructor( selector, apiController )
+	{
+		super();
+
+		this._selector      = selector;
+		this._apiController = apiController;
+	}
+
+	async load()
+	{
+		const responseData = await this._apiController.readUserSeriesWatched();
+
+		const container     = DomHelper.querySelector( this._selector );
+		container.innerHTML = responseData
+			.data
+			.seriesWatched
+			.sort(
+				( series_1, series_2 ) =>
+				{
+					if ( series_1.name < series_2.name )
+					{
+						return -1;
+					}
+					if ( series_1.name > series_2.name )
+					{
+						return 1;
+					}
+					return 0;
+				}
+			)
+			.map(
+				( series ) =>
+				{
+					return String.format`<li><a href="${ 0 }">${ 1 }</a></li>`( series.uri, series.name )
+				}
+			)
+			.join( '' );
+	}
+}
