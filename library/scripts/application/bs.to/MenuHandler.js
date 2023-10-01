7672a4ad998f54cb44eb9b1c2efb23ea41d1f564
Christian Ramelow <info@codekandis.net>
2023-10-01 08:11:56 +0200

    Closes menus on clicking outside of a menu.

--- library/scripts/application/bs.to/MenuHandler.js
+++ library/scripts/application/bs.to/MenuHandler.js
@@ -2,6 +2,8 @@
 
 class MenuHandler extends BaseClass
 {
+	static _menuState = null;
+
 	constructor( menuSettings )
 	{
 		super();
@@ -10,63 +12,84 @@ class MenuHandler extends BaseClass
 		this._menuStates   = [];
 	}
 
-	_addEventHandlers( menuSetting )
+	_hideMenu()
 	{
-		const menu      = DomHelper.querySelector( menuSetting.selector, document );
-		const menuState = {
-			menu:          menu,
-			loader:        menuSetting.loader,
-			menuActivator: DomHelper.querySelector(
-				String.format`${ 0 } ${ 1 }`( menuSetting.selector, menuSetting.activatorSelector ),
-				document
-			),
-			subMenu:       DomHelper.querySelector( 'ul', menu ),
-			isVisible:     false
-		};
-		this._menuStates.push( menuState );
-
-		DomHelper.addEventHandler(
-			menuState.menuActivator,
-			'click',
-			( event ) =>
-			{
-				event.preventDefault();
-
-				if ( false === menuState.isVisible )
-				{
-					this._menuStates.forEach(
-						( menuStateFetched ) =>
-						{
-							if ( true === menuStateFetched.isVisible )
-							{
-								menuStateFetched.isVisible             = false;
-								menuStateFetched.subMenu.style.display = 'none';
-								menuStateFetched.subMenu.innerHTML     = '';
-							}
-						}
-					);
-
-					menuState.isVisible             = true;
-					menuState.subMenu.style.display = 'block';
-					menuState.loader();
-
-					return;
-				}
-
-				menuState.isVisible             = false;
-				menuState.subMenu.style.display = 'none';
-				menuState.subMenu.innerHTML     = '';
-			}
-		);
+		if ( null !== MenuHandler._menuState )
+		{
+			MenuHandler._menuState.isVisible             = false;
+			MenuHandler._menuState.subMenu.style.display = 'none';
+			MenuHandler._menuState.subMenu.innerHTML     = '';
+			MenuHandler._menuState                       = null;
+		}
 	}
 
-	handle()
+	_showMenu( menuState )
+	{
+		MenuHandler._menuState                       = menuState;
+		MenuHandler._menuState.isVisible             = true;
+		MenuHandler._menuState.subMenu.style.display = 'block';
+		MenuHandler._menuState.loader();
+	}
+
+	_addMenuStateEventHandlers()
 	{
 		this._menuSettings.forEach(
 			( menuSetting ) =>
 			{
-				this._addEventHandlers( menuSetting );
+				const menu      = DomHelper.querySelector( menuSetting.selector, document );
+				const menuState = {
+					menu:          menu,
+					loader:        menuSetting.loader,
+					menuActivator: DomHelper.querySelector(
+						String.format`${ 0 } ${ 1 }`( menuSetting.selector, menuSetting.activatorSelector ),
+						document
+					),
+					subMenu:       DomHelper.querySelector( 'ul', menu ),
+					isVisible:     false
+				};
+				this._menuStates.push( menuState );
+
+				DomHelper.addEventHandler(
+					menuState.menuActivator,
+					'click',
+					( event ) =>
+					{
+						event.preventDefault();
+						event.stopPropagation();
+
+						if ( true === menuState.isVisible )
+						{
+							this._hideMenu();
+
+							return;
+						}
+
+						this._hideMenu();
+						this._showMenu( menuState );
+					}
+				);
 			}
 		);
+	}
+
+	_addDocumentEventHandlers()
+	{
+		DomHelper.addEventHandler( document, 'click', this._document_click );
+	}
+
+	_addEventHandlers()
+	{
+		this._addMenuStateEventHandlers();
+		this._addDocumentEventHandlers();
+	}
+
+	handle()
+	{
+		this._addEventHandlers();
+	}
+
+	_document_click = ( event ) =>
+	{
+		this._hideMenu();
 	}
 }
