899c91066f1e23ae32f086dd91305bad9d66a8d6
Christian Ramelow <info@codekandis.net>
2023-10-23 10:17:56 +0200

    Adds the `MenuFilterProvider`.

--- library/scripts/application/bs.to/BaseSeriesMenuLoader.js
+++ library/scripts/application/bs.to/BaseSeriesMenuLoader.js
@@ -6,33 +6,56 @@ class BaseSeriesMenuLoader extends BaseClass
 	{
 		super();
 
-		this._selector = selector;
+		this._selector      = selector;
+		this._menuContainer = null;
+	}
+
+	_appendMenuFilter()
+	{
+		const menuFilterItem = DomHelper.createElementFromString( '<li><input data-control-type="MENU_FILTER" type="text" placeholder="Filter"/></li>' );
+		const menuFilter     = DomHelper.querySelector( '[data-control-type="MENU_FILTER"]', menuFilterItem );
+		( new MenuFilterProvider( menuFilter, this._menuContainer ) )
+			.append();
+
+		DomHelper.appendChild( this._menuContainer, menuFilterItem );
+
+		menuFilter.focus();
+	}
+
+	_appendMenuItems( series )
+	{
+		const menuItems = DomHelper.createElementsFromString(
+			series
+				.sort(
+					( series_1, series_2 ) =>
+					{
+						if ( series_1.name < series_2.name )
+						{
+							return -1;
+						}
+						if ( series_1.name > series_2.name )
+						{
+							return 1;
+						}
+						return 0;
+					}
+				)
+				.map(
+					( series ) =>
+					{
+						return String.format`<li><a href="${ 0 }">${ 1 }</a></li>`( series.uri, series.name )
+					}
+				)
+				.join( '' )
+		);
+
+		DomHelper.appendChildren( this._menuContainer, menuItems );
 	}
 
 	_addSeries( series )
 	{
-		const container     = DomHelper.querySelector( this._selector );
-		container.innerHTML = series
-			.sort(
-				( series_1, series_2 ) =>
-				{
-					if ( series_1.name < series_2.name )
-					{
-						return -1;
-					}
-					if ( series_1.name > series_2.name )
-					{
-						return 1;
-					}
-					return 0;
-				}
-			)
-			.map(
-				( series ) =>
-				{
-					return String.format`<li><a href="${ 0 }">${ 1 }</a></li>`( series.uri, series.name )
-				}
-			)
-			.join( '' );
+		this._menuContainer = DomHelper.querySelector( this._selector );
+		this._appendMenuFilter();
+		this._appendMenuItems( series );
 	}
 }
