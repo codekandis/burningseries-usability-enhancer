8ab20ae8a8a5d89a1f3f2c39f999f6bbc33ee3f2
Christian Ramelow <info@codekandis.net>
2023-10-26 09:06:04 +0200

    Refactors the promise handling of the `BsToController`.

--- library/scripts/application/bs.to/BsToController.js
+++ library/scripts/application/bs.to/BsToController.js
@@ -11,90 +11,52 @@ class BsToController extends BaseClass
 
 	async readEpisodes( uri )
 	{
-		return await new Promise(
-			( resolveHandler, rejectHandler ) =>
-			{
-				this._ajaxController
-					.get( uri, [] )
-					.then(
-						( response ) =>
-						{
-							response
-								.text()
-								.then(
-									( htmlString ) =>
-									{
-										resolveHandler(
-											[
-												...DomHelper.querySelectorAll(
-													'table.episodes tbody tr td:nth-child( 1 ) a',
-													( new DOMParser() )
-														.parseFromString( htmlString, ContentTypes.TEXT_HTML ),
-													false
-												)
-											]
-										);
-									}
-								)
-						}
-					)
-			}
-		);
+		const htmlString = await (
+			await this
+				._ajaxController
+				.get( uri, [] )
+		)
+			.text();
+
+		return [
+			...DomHelper.querySelectorAll(
+				'table.episodes tbody tr td:nth-child( 1 ) a',
+				( new DOMParser() )
+					.parseFromString( htmlString, ContentTypes.TEXT_HTML ),
+				false
+			)
+		];
 	}
 
 	async readWatchStates( uri )
 	{
-		return await new Promise(
-			( resolveHandler, rejectHandler ) =>
-			{
-				this._ajaxController
-					.get( uri, [] )
-					.then(
-						( response ) =>
-						{
-							response
-								.text()
-								.then(
-									( htmlString ) =>
-									{
-										resolveHandler(
-											[
-												...DomHelper.querySelectorAll(
-													'table.episodes tr',
-													( new DOMParser() )
-														.parseFromString( htmlString, ContentTypes.TEXT_HTML ),
-													false
-												)
-											]
-												.map(
-													( episode ) =>
-													{
-														return true === episode.classList.contains( 'watched' );
-													}
-												)
-										);
-									}
-								)
-						}
-					)
-			}
-		);
+		const htmlString = await (
+			await this
+				._ajaxController
+				.get( uri, [] )
+		)
+			.text();
+
+		return [
+			...DomHelper.querySelectorAll(
+				'table.episodes tr',
+				( new DOMParser() )
+					.parseFromString( htmlString, ContentTypes.TEXT_HTML ),
+				false
+			)
+		]
+			.map(
+				( episode ) =>
+				{
+					return true === episode.classList.contains( 'watched' );
+				}
+			);
 	}
 
 	async toggleWatchState( uri )
 	{
-		return await new Promise(
-			( resolveHandler, rejectHandler ) =>
-			{
-				this._ajaxController
-					.get( uri, [] )
-					.then(
-						( response ) =>
-						{
-							resolveHandler();
-						}
-					)
-			}
-		);
+		return await this
+			._ajaxController
+			.get( uri, [] );
 	}
 }
