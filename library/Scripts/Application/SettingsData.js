9792c5ce7e206a5ef66d8a141125e8d8601089ac
Christian Ramelow <info@codekandis.net>
2024-09-16 11:43:52 +0200

    Extends default player to multiple default players.

--- library/Scripts/Application/SettingsData.js
+++ library/Scripts/Application/SettingsData.js
@@ -6,5 +6,5 @@ class SettingsData extends BaseClass
 	apiUserId         = '00000000-0000-0000-0000-000000000000';
 	apiKey            = '1234';
 	preferredLanguage = 'de';
-	defaultPlayer     = 'Streamtape';
+	defaultPlayers    = 'Streamtape';
 }
