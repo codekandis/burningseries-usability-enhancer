04c4169840c9d42287c0d9bf076e0296a17c399a
Christian Ramelow <info@codekandis.net>
2024-09-16 12:05:02 +0200

    Adds `LinkExtensionTypes`.

---
+++ library/Scripts/Application/LinkExtensionTypes.js
@@ -0,0 +1,14 @@
+'use strict';
+
+class LinkExtensionTypes extends StaticBaseClass
+{
+	static get DEFAULT_LANGUAGE()
+	{
+		return 'defaultLanguage';
+	}
+
+	static get PREFERRED_PLAYER()
+	{
+		return 'preferredPlayer';
+	}
+}
